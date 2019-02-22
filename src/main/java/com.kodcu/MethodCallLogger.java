ace7113f81f1ec3a48ffe82c44c2421c1d603a21
altuga@gmail.com <upux1234>
2019-02-22 23:21:41 +0300

    13 is added

--- src/main/java/com.kodcu/MethodCallLogger.java
+++ src/main/java/com.kodcu/MethodCallLogger.java
@@ -10,6 +10,16 @@ public class MethodCallLogger {
     public Object log(InvocationContext ic) throws Exception{
 
         Method method = ic.getMethod();
+        Object[] objects = ic.getParameters();
+        if (objects != null) {
+            for (int i = 0; i < objects.length; i++) {
+                Object object = objects[i];
+                System.out.println(object);
+            }
+        }
+
+        System.out.println(" ic.getTarget() : " + ic.getTarget());
+
         long start = System.nanoTime();
         try {
             return ic.proceed();
